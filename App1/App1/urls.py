78aa84836dd5189ae1a7c10325ed90de1fe91c2e
DevAsper <asponomaryov@yandex.ru>
2024-07-12 13:56:21 +0300

    update with news app

--- App1/App1/urls.py
+++ App1/App1/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from myapp.views import data_view, test_view, page1_view, page2_view
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('test/', test_view, name='test'),
     path('page1/', page1_view, name='page1'),
     path('page2/', page2_view, name='page2'),
+    path('news/', include('news.urls'))
 ]
---
+++ App1/news/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from .models import News
+
+def news_list(request):
+    news_list = News.objects.all()
+    return render(request, 'news/news_list.html', {'news_list': news_list})
+
+def news_detail(request, news_id):
+    news = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/news_detail.html', {'news': news})
+
+
